02b0fec5f5168734b3f5f1ba08dee6adc974ef27
Josias Roberto <josiasroberto1@gmail.com>
2022-10-24 19:05:19 -0300

    Partials and database added

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const app = express()
+const connection = require('./database/database')
+const Pergunta  = require('./database/Pergunta')
+
+//database
+connection.authenticate().then(()=>{
+  console.log('Conexão feita com o banco de dados!')
+}).catch((err)=>{
+  console.log(err)
+})
+
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 app.get('/',(req,res)=>{
-  res.render('./index')
+  Pergunta.findAll({raw:true}).then((perguntas)=>{
+    res.render('./index', {perguntas:perguntas})
+    
+  })
 })
 
 app.get('/perguntar',(req,res)=>{
   res.render('./perguntar')
+})
+
+app.post('/salvarpergunta',(req,res)=>{
+    
+  Pergunta.create({
+    titulo: req.body.titulo,
+    descricao: req.body.descricao
+  }).then(()=>{
+    res.redirect('/')
+  })
+  
 })
 
 
